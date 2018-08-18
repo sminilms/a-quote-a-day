e7f5cf0a6b5bcb8b002297aabe24e645fb1f7e5f
sminilms <sminilms@hotmail.com>
2018-08-19 00:36:31 +0300

    Made all changes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.get('/', function(request,response){
     response.sendFile(__dirname+"/index.html");
 });
 
-var port = process.env || 8080;
+var port = process.env.port || 8080;
 
 var server = app.listen(port, function(req,res){
     console.log("Catch the action at http://localhost:"+port);
-})
+});
